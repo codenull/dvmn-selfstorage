4ac1a449da6b2c83f6f3f11f4c5f6d5da67c70ef
Shirlusha <sheerly.morra@gmail.com>
2021-11-17 17:27:26 +0300

    Add base template

--- storage/views.py
+++ storage/views.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-def view_main(request):
-    context = {}
-    return render(request, 'index.html', context)
+def show_index(request):
+    return render(request, 'index.html')
+
+
+def show_season(request):
+    return render(request, 'season.htlml')
+
+
+def show_checkout(request):
+    return render(request, 'checkout.html')
