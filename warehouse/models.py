175845688da5ce4f4b9daff3baf03c9632d894f0
stas <garanzha.s.a@gmail.com>
2021-11-17 17:20:51 +0300

    create warehouse app and add models to it

---
+++ warehouse/models.py
@@ -0,0 +1,69 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+Client = get_user_model()
+
+
+class Storage(models.Model):
+    address = models.CharField(max_length=150)
+    longitude = models.FloatField(verbose_name='долгота',
+                                  blank=True,
+                                  null=True)
+    latitude = models.FloatField(verbose_name='широта',
+                                 null=True,
+                                 blank=True)
+    description = models.TextField(blank=True,
+                                   verbose_name='короткое описание')
+    picture = models.ImageField(verbose_name='изображение')
+    first_square_meter_price = models.PositiveIntegerField(
+        verbose_name='цена за первый метр'
+    )
+    rest_meters_price = models.PositiveIntegerField(
+        verbose_name='цена за каждый последующий метр'
+    )
+    stored_items = models.ManyToManyField(to='Inventory',
+                                          through='InventoryPriceList',
+                                          verbose_name='хранимые вещи')
+
+    class Meta:
+        verbose_name = 'склад'
+        verbose_name_plural = 'склады'
+
+
+class Inventory(models.Model):
+    name = models.CharField(max_length=100,
+                            unique=True,
+                            verbose_name='название')
+    picture = models.ImageField(verbose_name='изображение инвентаря')
+
+    class Meta:
+        verbose_name = 'инвентарь для хранения'
+        verbose_name_plural = 'хранимые вещи'
+
+
+class InventoryPriceList(models.Model):
+    storage = models.ForeignKey(to=Storage, verbose_name='склад')
+    inventory = models.ForeignKey(to=Inventory, verbose_name='инвентарь')
+    price_per_week = models.PositiveIntegerField(verbose_name='цена за неделю')
+    price_per_month = models.PositiveIntegerField(verbose_name='цена за месяц')
+
+
+class BoxRentalOrder(models.Model):
+    client = models.ForeignKey(to=Client,
+                               related_name='box_rental_orders',
+                               verbose_name='клиент')
+    storage = models.ForeignKey(to='Storage',
+                                related_name='clients',
+                                verbose_name='склад')
+    box_size = models.IntegerField(verbose_name='размер бокса',
+                                   null=True,
+                                   blank=True)
+    inventory = models.ManyToManyField(to='Inventory',
+                                       verbose_name='вещи для хранения')
+    start_date = models.DateField(verbose_name='дата начала услуг')
+    end_date = models.DateField(verbose_name='дата окончания услуг')
+
+    class Meta:
+        verbose_name = 'заказ'
+        verbose_name_plural = 'заказы'
